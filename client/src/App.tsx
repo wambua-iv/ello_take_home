b184be6fa7a001179b385705a2166f48a5bf1b60
musalu <musaluwambua@gmail.com>
2023-10-01 02:21:22 +0300

    feat: client routes declaration

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,8 @@
 import { QueryClient, QueryClientProvider } from 'react-query'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Landing from './components/Landing'
 import SingleWordView from './components/WordPage'
 import BookPages from './components/BookPages'
+import Landing from './components/Landing'
 
 function App() {
   const queryClient = new QueryClient
---
+++ client/src/components/Landing/index.tsx
@@ -0,0 +1,9 @@
+import React from "react"
+
+const Landing: React.FC = () => {
+    return(
+        <></>
+    )
+}
+
+export default Landing
