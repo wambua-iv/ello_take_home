20af696e96d851766a86ded93bc013ccf26a3774
musalu <musaluwambua@gmail.com>
2023-10-01 02:22:34 +0300

    refactor: vite config for image creation

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+    //setup for docker image creation
+
+    server:{
+      watch: {
+        usePolling: true,
+      },
+      host: true,
+      port: 4090
+    }
 })
