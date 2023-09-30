025a22c893922e028b89c98ab1c6769de8d735c1
musalu <musaluwambua@gmail.com>
2023-10-01 02:09:37 +0300

    feat: book data type definition

--- client/src/components/BookPages/index.ts
+++ client/src/components/BookPages/index.ts
@@ -1,38 +1,52 @@
 const BookPages: React.FC = () => {
-    const { book_number } = useParams();
-  
-      const [bookData, setBookData] = React.useState<Data>({    book: {
-          title: "",
-          author: "",
-          book_pages: [
+  type Data = {
+    book: {
+      title: string;
+      author: string;
+      book_pages: {
+        pageIndex: number;
+        content: string;
+        tokens: {
+          position: number[];
+          value: string;
+        }[];
+      }[];
+    };
+  };
+
+  const { book_number } = useParams();
+
+  const [bookData, setBookData] = React.useState<Data>({
+    book: {
+      title: "",
+      author: "",
+      book_pages: [
+        {
+          pageIndex: 1,
+          content: "",
+          tokens: [
             {
-              pageIndex: 1,
-              content: "",
-              tokens: [
-                {
-                  position: [34, 23],
-                  value: "",
-                },
-              ],
+              position: [34, 23],
+              value: "",
             },
           ],
         },
-      });
+      ],
+    },
+  });
 
-      const { isLoading } = useQuery(
-          "Query",
-          async () =>
-            await fetch(books_endpoint, {
-              method: "POST",
-              headers: { "Content-Type": "application/json" },
-              body: JSON.stringify({ query: get_book_query }),
-            })
-              .then((data) => data.json())
-              .then((data) => setBookData(data.data))
-        );
-        if(isLoading) return <div>loading</div>
-      return(
-          !isLoading && <BookPage book={bookData.book}/>
-      )
-  }
-  export default BookPages
+  const { isLoading } = useQuery(
+    "Query",
+    async () =>
+      await fetch(books_endpoint, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ query: get_book_query }),
+      })
+        .then((data) => data.json())
+        .then((data) => setBookData(data.data))
+  );
+  if (isLoading) return <div>loading</div>;
+  return !isLoading && <BookPage book={bookData.book} />;
+};
+export default BookPages;
