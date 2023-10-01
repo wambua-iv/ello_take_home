40d9e90064f3915c1ca09a3c4170bb93f0294217
musalu <musaluwambua@gmail.com>
2023-10-01 12:03:01 +0300

    fix: terraform cloud provider field

--- main.tf
+++ main.tf
@@ -35,7 +35,7 @@ resource "aws_vpc" "default" {
 
 
 resource "aws_subnet" "public" {
-  count                   = 2
+  count                   = 1
   cidr_block              = cidrsubnet(aws_vpc.default.cidr_block, 8, 2 + count.index)
   availability_zone       = data.aws_availability_zones.available_zones.names[count.index]
   vpc_id                  = aws_vpc.default.id
