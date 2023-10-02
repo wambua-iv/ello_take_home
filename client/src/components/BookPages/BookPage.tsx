a15ad64a6b77357c2b874cb22eec37155411d911
Wambua Musalu <musaluwambua@gmail.com>
2023-10-02 13:10:40 +0300

    fix: clickable content

--- client/src/components/BookPages/BookPage.tsx
+++ client/src/components/BookPages/BookPage.tsx
@@ -28,49 +28,62 @@ const ClickableWord: React.FC<ContentProps> = ({ content, tokens }) => {
       <div className="clickable">
         {content
           .match(/.*?[,?!".?”\s-, ]|[\w+]+/g)
-          ?.map((word: string, i: number) => (
-            <a
-              key={i}
-              href={`/word/${word
-                .replace(/[.,?/#!$%^&*;:{}=\-_`~()"“” ]/g, "")
-                .toLowerCase()}`}
-            >
-              {
-                //enforcing mapping of tokens to page content and displaying
-                tokens.find(
-                  (element) =>
-                    (element.value ==
-                      word
-                        .replace(/[.,?/#!$%^&*;:{}=\-_`~()"“” ]/g, "")
-                        //punctuation affecting comparison  => force switch of punctuation mark
-                        .replace("’", "'")
-                        .toLowerCase() &&
-                      element.value ==
-                        content.slice(
-                          element.position[0],
-                          element.position[1]
-                        )) ||
-                    "”" //=> added to maintain punctuation order
-                )
-                  ? word //display word to screem
-                  : " " // maintain word spacing
-              }
-            </a>
-          ))}
+          ?.map((word: string, i: number) =>
+            //enforcing mapping of tokens to page content and displaying
+            tokens.find(
+              (element) =>
+                (element.value ==
+                  word
+                    .replace(/[.,?/#!$%^&*;:{}=\-_`~()"“” ]/g, "")
+                    //punctuation affecting comparison  => force switch of punctuation mark
+                    .replace("’", "'")
+                    .toLowerCase() &&
+                  element.value ==
+                    content.slice(element.position[0], element.position[1])) ||
+                " "
+            )
+              ? word
+                  .match(/[.,?/#!$%^&*;:{}=\-_`~()"“” ]|[\w+]/)
+                  ?.map((letter) =>
+                    word == letter ? (
+                      //make sure punctuation marks are not clickable
+                      <span
+                        key={i}
+                        style={{
+                          pointerEvents: "none",
+                          cursor: "default",
+                          color: "#333222",
+                          fontSize: "32px",
+                        }}
+                      >
+                        {letter}
+                      </span>
+                    ) : (
+                      <a
+                        key={i}
+                        href={`/word/${word
+                          .replace(/[.,?/#!$%^&*;:{}=\-_`~()"“” ]/g, "")
+                          .toLowerCase()}`}
+                      >
+                        {word}
+                      </a>
+                    )
+                  )
+              : null
+          )}
       </div>
     </>
   );
 };
 
 const BookPage: React.FC<PageProps> = ({ book }) => {
-  const [displayCount, setDisplayCount] = React.useState<number>(1);
-  const start = displayCount > 1 ? displayCount : 0;
-  const end = displayCount > 1 ? displayCount : 0;
+  const [displayCount, setDisplayCount] = React.useState<number>(0);
+  const start = displayCount;
   return (
     <>
       <div className="pages">
         {book.book_pages
-          .slice(start, end + 2)
+          .slice(start, displayCount + 2)
           .map((item: ContentProps, i: number) => (
             <div className="pages_page" key={i}>
               <ClickableWord content={item.content} tokens={item.tokens} />
@@ -78,28 +91,24 @@ const BookPage: React.FC<PageProps> = ({ book }) => {
           ))}
       </div>
       <div className="pages_buttons">
-        {!(book.book_pages.length + 1 == displayCount) && (
-          <button
-            className="pages_buttons_button"
-            onClick={() =>
-              setDisplayCount((prev: number) =>
-                book.book_pages.length > prev
-                  ? prev + 2
-                  : book.book_pages.length
-              )
-            }
-          >
-            +
-          </button>
-        )}
-        {!(displayCount <= 2) && (
-          <button
-            className="pages_buttons_button"
-            onClick={() => setDisplayCount((prev: number) => prev - 2)}
-          >
-            -
-          </button>
-        )}
+        <button
+          disabled={displayCount < 1 ? true : false}
+          className="pages_buttons_button"
+          onClick={() => setDisplayCount((prev: number) => prev - 2)}
+        >
+          -
+        </button>
+        <button
+          disabled={book.book_pages.length == displayCount ? true : false}
+          className="pages_buttons_button"
+          onClick={() =>
+            setDisplayCount((prev: number) =>
+              book.book_pages.length > prev ? prev + 2 : book.book_pages.length
+            )
+          }
+        >
+          +
+        </button>
       </div>
     </>
   );
